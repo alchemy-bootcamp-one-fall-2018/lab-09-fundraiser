a954804ff1c5b292e588b585a774aa2f968ab6ad
Kristin Hortsch <kristinhortsch@gmail.com>
2018-11-01 16:08:57 -0700

    product-api is linked to app.js and successfully logged on console

--- js/products-api.js
+++ js/products-api.js
@@ -1,1 +1,35 @@
-let products = 
+let products = [
+    {
+        name: 'Collar',
+        price: '$10.00',
+        description: 'reflective collar to keep your doggo safe',
+        animal: 'dog'
+    }
+];
+
+function saveProducts() {
+    localStorage.setItem('products', JSON.stringify(products));
+}
+
+const productsApi = {
+    getAll() {
+        const json = localStorage.getItem('products');
+        if(json) {
+            products = JSON.parse(json);
+        }
+        return products;
+    },
+    add(product) {
+        products.push(product);
+        saveProducts();
+    },
+    remove(product) {
+        const index = products.indexOf(product);
+        if(index !== -1) {
+            products.splice(index, 1);
+            saveProducts();
+        }
+    }
+};
+
+export default productsApi;
