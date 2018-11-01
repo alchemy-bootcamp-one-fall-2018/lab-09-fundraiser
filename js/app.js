3451b1190bb343b93b38a61c0eae8977427002fb
Tyler Corbett <thetylercorbett@gmail.com>
2018-11-01 16:55:22 -0700

    worked on cookies list, app.js, order.html

--- js/app.js
+++ js/app.js
@@ -1,1 +1,4 @@
+import cookieApi from './cookies-api.js';
+import cookieList from './cookie-list.js';
 
+const cookies = cookieApi.getAll();
---
+++ js/cookie-list.js
@@ -0,0 +1,35 @@
+
+
+function makeCookie(cookie) {
+    const html = /*html*/ `
+        <li class="cookie">
+            <h3 class="name">
+                ${cookie.name}
+            </h3>
+        </li>
+        `;
+
+    const template = document.createElement('template');
+
+    template.innerHTML = html;
+
+    return template.content;
+}
+
+const list = document.getElementById('cookies');
+
+const cookieList = {
+    init(cookies) {
+        for(let i = 0; i < cookies.length; i++) {
+            cookieList.add(cookies[i]);
+        }
+        cookieList.cookies = cookies;
+
+    },
+    add(cookie) {
+        const dom = makeCookie(cookie);
+        list.appendChild(dom);
+    }
+};
+
+export default cookieList;
