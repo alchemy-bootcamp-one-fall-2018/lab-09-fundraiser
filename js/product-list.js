30c068708790d1a18e18a69770bda5f23a9ae06e
Aaron Dennis <AMDennis1987@gmail.com>
2018-11-01 15:38:09 -0700

    productList

--- js/product-list.js
+++ js/product-list.js
@@ -1,6 +1,35 @@
-function productsList() {
-    console.log('it works');
+import html from './html.js';
+
+function addProduct(product) {
+    const html =  `
+
+    `;
+
+    const template = document.createElement('template');
+    
+    template.innerHTML = html;
+
+    return template.content;
 }
+
+
+
+const list = document.getElementById('products');
+
+const productsList = {
+    init(products) {
+        for(let i = 0; i < products.length; i++) {
+            const dom = addProduct(products[i]);
+            list.appendChild(dom);
+        }
+
+    },
+    add(product) {
+        const dom = 
+    }
+};
+
+
 
 
 export default productsList;
