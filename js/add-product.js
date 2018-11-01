a043647cb02c9e9b8fad661b2bf1fbe65478e1d5
Kristin Hortsch <kristinhortsch@gmail.com>
2018-11-01 16:42:31 -0700

    admin-producted tested with inputs and successfully links to prodcutApi

---
+++ js/add-product.js
@@ -0,0 +1,24 @@
+const addProduct = {
+    init(onAdd) {
+        const form = document.getElementById('product-form');
+        form.addEventListener('submit', function(event) {
+            event.preventDefault();
+
+            const elements = form.elements;
+
+            const product = {
+                name: elements.name.value,
+                price: elements.price.value,
+                description: elements.description.value,
+                animal: elements.animal.value
+            };
+
+            onAdd(product);
+
+            form.reset();
+            document.activeElement.blur();
+        });
+    }
+};
+
+export default addProduct;
--- js/app.js
+++ js/app.js
@@ -1,5 +1,10 @@
 import productApi from './products-api.js';
+import addProduct from './add-product.js';
 
 const products = productApi.getAll();
+
+addProduct.init(function(product) {
+    productApi.add(product);
+});
 
 console.log(products);
