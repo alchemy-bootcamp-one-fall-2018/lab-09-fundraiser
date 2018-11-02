5a93ee1e703d517c4ecbf7911af4c41fd8a015f9
Marcy Silverman <marcysilve225@gmail.com>
2018-11-02 15:32:05 -0700

    cartapi and order app is done

--- js/cart-api.js
+++ js/cart-api.js
@@ -1,6 +1,34 @@
-import html from './html.js'; 
+let cart = []; 
 
-function makeItem(item) {
-    return html`
-        <li 
+function saveCart() {
+    localStorage.setItem('cart', JSON.stringify(cart)); 
 }
+
+const cartApi = {
+    get() {
+        const json = localStorage.getItem('cart'); 
+        if(json) {
+            cart = JSON.parse(json); 
+        }
+        return cart; 
+    },
+    add(product) {
+        const orderItem = cart.find(item => {
+            return item.name === product.name;
+        });
+        
+        if(orderItem) {
+            orderItem.quantity++; 
+        }
+        else {
+            cart.push({
+                name: product.name, 
+                quantity: 1
+            }); 
+        }
+
+        saveCart(); 
+    }
+}; 
+
+export default cartApi; 
