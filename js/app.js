1fa15132d474632412aec3a81fee4b8e39dce61c
Abel Quintero <abel.j.quintero@gmail.com>
2018-11-02 15:36:03 -0700

    starting product detail js

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 import addProduct from './add-product.js';
 import productList from './product-list.js';
 import productApi from './products-api.js';
-// import productDetail from './product-detail.js';
+import productDetail from './product-detail.js';
 
 const products = productApi.getAll();
 console.log(products);
@@ -12,11 +12,11 @@ productList.init(products, /*onSelect*/ function(product){
     console.log(product);
 });
 
-// productDetail.init(/*onRemove*/ function(product){
-// const index = productApi.remove(product);
-// // tell the product list to re-render
-//     productList.remove(index);
-// });
+productDetail.init(/*onRemove*/ function(product){
+    const index = productApi.remove(product);
+// tell the product list to re-render
+    productList.remove(index);
+});
 
 addProduct.init(function(product){
 
