d1a6e02e8d135fb32006dd261f7f94601ceaa82a
Tyler Corbett <thetylercorbett@gmail.com>
2018-11-02 12:28:28 -0700

    slight changes. changing roles

--- js/add-cookie.js
+++ js/add-cookie.js
@@ -1,19 +1,13 @@
 const addCookie = {
-  // "init" method for initializing the component
-  // 1. passed data or callback functions
-  // 2. setup some event listeners
+ 
     init(onAdd) {
         const form = document.getElementById('add-form');
 
-      // "standard" way to add event listeners in JavaScript
         form.addEventListener('submit', function(event) {
-          // prevent form from reloading page
+
             event.preventDefault();
-
-          // form elements
+            
             const elements = form.elements;
-
-          // get the values from the form controls
             const cookie = {
                 id: elements.id.value,
                 name: elements.name.value,
--- js/order-app.js
+++ js/order-app.js
@@ -5,14 +5,5 @@ import cookieList from './cookie-list.js';
 const cookies = cookieApi.getAll();
 
 
-addCookie.init(function(cookie) {
-  // tell the api service first
-    cookieApi.add(cookie);
-
-});
 
 cookieList.init(cookies);
-
-
-
-
