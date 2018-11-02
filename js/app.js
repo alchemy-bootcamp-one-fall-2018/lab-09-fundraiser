a19c14f165285a0948a5c6fc1ff38128d0902f82
Marcy Silverman <marcysilve225@gmail.com>
2018-11-01 17:46:28 -0700

    product admin list js is done

--- js/app.js
+++ js/app.js
@@ -1,8 +1,8 @@
 import addProduct from './products.js'; 
 import productsApi from './product-api.js'; 
+// import productList from './product-admin-list.js'; 
 
-//const products = productsApi.getAll();
-
+// const products = productsApi.getAll();
 
 addProduct.init(function(product) {
 
