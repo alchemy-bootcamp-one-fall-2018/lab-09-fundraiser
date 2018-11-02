cd41ac93d05fa568532b6681cb8b2ac59508ecf9
Phoebe LaBlonde <pmlablonde2220@gmail.com>
2018-11-02 15:02:03 -0700

    working on products api

--- js/product.js
+++ js/product.js
@@ -1,25 +1,30 @@
+//import productApi from './products-api.js';
+
 const addCart = {
     
-    init(onAdd){
+    init(){
         const productForm = document.getElementById('donation-form');
-        
         productForm.addEventListener('submit', function(event) {
             event.preventDefault();
-
+            
             const elements = productForm.elements;
+            console.log(elements);
             
             const donationValue = {
-
-                total: elements.value
-
+                
+                total: elements.donation.value
+                
             };
             onAdd(donationValue);
-
+            
             productForm.reset();
         });
     }
-
+    
 };
+addCart.init();
 
-export default addCart;
+//export default addCart;
 
+
+//event listener is triggered when we hit the submit, which then runs the anonymous function, which is defined from lines 7-22
---
+++ js/products-api.js
@@ -0,0 +1,14 @@
+let products = [1, 2, 3];
+
+
+const productsApi = {
+    getProducts() {
+        return products;
+    } 
+
+};
+
+
+
+
+export default productsApi;
