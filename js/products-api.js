7c959a71c17c1bc5817a056574d8aa628bbbe155
Lance Owen Merrill <Lanceom@hawaii.edu>
2018-11-02 12:57:46 -0700

    filled in products api page

---
+++ js/products-api.js
@@ -0,0 +1,42 @@
+let products = [
+    { 
+        id: 'carrot',
+        name: 'Carrot',
+        description: 'long orange goodness',
+        price: 5,
+    },
+
+    { 
+        id: 'canned goods',
+        name: 'canned foods',
+        description: 'random types of canned goods',
+        price: 10,
+    }
+];
+function saveProducts() {
+    localStorage.setItem('products',JSON.stringify(products)); }
+
+
+const productApi = {
+    getAll() {
+        const json = localStorage.getItem('products');
+        if(json) {
+            products = JSON.parse(json);
+        }
+        return products;
+    },
+    add(product){
+        products.push(product);
+        saveProducts();
+    },
+
+    remove(product) {
+        const index = products.indexOf(product);
+        if(index !== -1) {
+            products.splice(index, 1);
+            saveProducts();
+            return index;
+        }
+    }
+};
+export default productApi;
