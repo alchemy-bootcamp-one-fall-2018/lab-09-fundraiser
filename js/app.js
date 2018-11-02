114676acedcf8fb2b76e15a3e9d411d815fa883f
Lise Brackbill <menagerie@hevanet.com>
2018-11-01 17:38:28 -0700

    hit switch driver time

--- js/app.js
+++ js/app.js
@@ -1,9 +1,11 @@
 import addProduct from './components/add-product.js';
 import productsApi from './data-apis/products-api.js';
+import productList from './components/product-list.js';
 
 addProduct.init(function(product) {
     productsApi.add(product);
-    // productList.add(product);
+    productList.add(product);
 });
 
-const products = productsApi.getAll();
+// const products = productsApi.getAll();
+
---
+++ js/components/product-list.js
@@ -0,0 +1,41 @@
+import html from '../helper-utilities/html.js';
+
+function makeProduct(product) {
+    const html = `
+        <li class="product">
+            <h3 class="name">
+            ${product.name}
+            </h3>
+            <img src="assets/${product.image ? product.image : 'TKO.jpg'}">
+        </li>
+        `;
+
+    const template = document.createElement('template');
+
+    template.innerHTML = html;
+
+    return template.content;
+
+}
+
+const list = document.getElementById('products');
+
+const productList = {
+    init(products, onSelect) {
+        for(let i = 0; i < products.length; i++) {
+            productList.add(products[i]);
+        }
+        productList.onSelect = onSelect; 
+        productList.products = products;
+    },
+    add(product) {
+        const dom = makeProduct(product);
+        const listItem = dom.querySelector('li');
+        listItem.addEventListener('click', function() {
+            productList.onSelect(product);
+        });
+        list.appendChild(dom);
+    }
+}; 
+
+export default productList;
