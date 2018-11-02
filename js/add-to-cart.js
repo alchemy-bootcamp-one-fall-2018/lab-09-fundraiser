80cef384b4fe7fb88bf1832917fc488284007163
Shaba9 <shaba92@gmail.com>
2018-11-02 14:37:21 -0700

    displays correctly, but not on page load yet

--- js/add-to-cart.js
+++ js/add-to-cart.js
@@ -1,8 +1,10 @@
 import productsApi from './products-api.js';
 
-let cartItems = [];
+let cartItems = JSON.parse(localStorage.getItem('cart'));
 const products = productsApi.getProducts();
 let total;
+
+// displayCart();
 
 function addItems(buttonId) {
     let selectedProduct = products[parseInt(buttonId)];
@@ -11,7 +13,6 @@ function addItems(buttonId) {
     } else {
         cartItems = [selectedProduct];
     }
-    storeCart();
 }
 
 function storeCart() {
@@ -20,31 +21,32 @@ function storeCart() {
 }
 
 function displayCart() {
-    const cartAside = JSON.parse(localStorage.getItem('cart'));  
+    const cartAside = JSON.parse(localStorage.getItem('cart'));
     const sideItems = document.getElementById('sideItems');  
-    let html;
+    let html = '';
     
-    cartAside.forEach(function(item) {
-        total = JSON.parse(localStorage.getItem('cartTotal'));
-        total += item.price;
+    if(cartAside) {
+        cartAside.forEach(function(item) {
+            total = JSON.parse(localStorage.getItem('cartTotal'));
+            total += item.price;
+            
+            html += `
+            <p>Item: ${item.name}</p>
+            <p>Price: ${item.price}</p><br /><br /><br />     
+            `;
+        });
+        html += `<p>Total: ${total}</p>`;
+        localStorage.setItem('cartTotal', JSON.stringify(total));
         
-        html += `
-        <p>Item: ${item.name}</p>
-        <p>Price: ${item.price}</p><br /><br /><br />     
-        `;
-    });
-    html += `<p>Total: ${total}</p>`;
-    localStorage.setItem('cartTotal', JSON.stringify(total));
-    
-    sideItems.innerHTML = html;
-    
-    
+        sideItems.innerHTML = html;
+    }   
 }
 
 
 const addToCart = {
     init(buttonId) {
         addItems(buttonId);
+        storeCart();
     }
 };
 
