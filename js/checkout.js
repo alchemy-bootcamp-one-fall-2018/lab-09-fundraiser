d33f7272cd0d5302844edd759428869941e74211
Ivan <i.poblete915@gmail.com>
2018-11-02 16:41:19 -0700

    cart displays on user end on one line

--- js/checkout.js
+++ js/checkout.js
@@ -1,4 +1,20 @@
 import cartApi from './cart-api.js';
+
 let cart = cartApi.get();
+const orderSummary = document.getElementById('summary');
+let orders = [];
+const getCart = {
+    display(cart) {
+        for(let i = 0; i < cart.length; i++) {
+            orders.push(cart[i].name + ' ' + cart[i].price + ' ' + cart[i].quantity);
+            for(let i = 0; i < orders.length; i++) {
+                orderSummary.textContent = orders;
+            }    
+        }
+    }
+};
+
+getCart.display(cart);
+
 
 console.log(cart);
