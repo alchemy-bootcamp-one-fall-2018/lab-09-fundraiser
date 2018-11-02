3908177b8be138b2198ea39d4e4954f64de4283a
Phoebe LaBlonde <pmlablonde2220@gmail.com>
2018-11-02 12:22:24 -0700

    worked on products list function

---
+++ js/product.js
@@ -0,0 +1,18 @@
+var donation20 = document.getElementById('20-donation');
+var donation40 = document.getElementById('40-donation');
+var donation60 = document.getElementById('60-donation');
+
+const form = document.getElementById('donation-form');
+
+const cart = [];
+
+
+for(i=0; i< donationArray.length; i++) {
+    if(donationArray[i].value) {
+        cart.push(donationArray[i])
+    } 
+
+
+
+}
+
