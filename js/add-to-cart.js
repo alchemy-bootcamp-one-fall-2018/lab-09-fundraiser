876271473b205a68195246921cbfc7d81a34bcb1
Shaba9 <shaba92@gmail.com>
2018-11-02 16:09:58 -0700

    added remove buttons

--- js/add-to-cart.js
+++ js/add-to-cart.js
@@ -3,18 +3,21 @@ import productsApi from './products-api.js';
 let cartItems = JSON.parse(localStorage.getItem('cart'));
 const products = productsApi.getProducts();
 let total;
-
-displayCart();
+let cartItemCount;
 
 function addItems(buttonId) {
     let selectedProduct = products[parseInt(buttonId)];
+    // console.log(selectedProduct);
+    selectedProduct.cartId = cartItemCount - 1;
     if(cartItems) {
         cartItems.push(selectedProduct);
     } else {
         cartItems = [selectedProduct];
     }
-
+    
     storeCart();
+    cartItemCount = cartItems.length;
+    console.log(cartItemCount);
     addTotal();
 }
 
@@ -29,38 +32,52 @@ function addTotal(){
         total = 0;
     }
     localStorage.setItem('cartTotal', JSON.stringify(total));
-    displayCart();
+    addToCart.displayCart();
 }
 
 function storeCart() {
     localStorage.setItem('cart', JSON.stringify(cartItems));
 }
 
-function displayCart() {
-    const cartAside = JSON.parse(localStorage.getItem('cart'));
-    const sideItems = document.getElementById('sideItems');  
-    let html = '';
-    
-    if(cartAside) {
-        cartAside.forEach(function(item) {
-            total = JSON.parse(localStorage.getItem('cartTotal'));
-                       
-            html += `
-            <p>Item: ${item.name}</p>
-            <p>Price: $${item.price}</p><br /><br /><br />     
-            `;
-        });
-        html += `<p>Total: ${total}</p>`;
-
-        sideItems.innerHTML = html;
-    }   
-}
-
-
 const addToCart = {
     init(buttonId) {
         addItems(buttonId);
+    },
+
+    displayCart() {
+        const cartAside = JSON.parse(localStorage.getItem('cart'));
+        const sideItems = document.querySelector('.sideItems');  
+        let html = '';
+        
+        if(cartAside) {
+            cartAside.forEach(function(item) {
+                total = JSON.parse(localStorage.getItem('cartTotal'));
+                            
+                html += `
+                <p>Item: ${item.name}</p>
+                <p>Price: $${item.price}</p>
+                <button class="remove" id="${item.cartId}">Remove</button><br /><br /><br />     
+                `;
+            });
+            html += `<p>Total: ${total}</p>`;
+    
+            sideItems.innerHTML = html;
+        }   
+
+        const removeButton = document.querySelectorAll('.remove');
+
+        removeButton.forEach(function(button) {
+            button.addEventListener('click', function() {
+                addToCart.removeItem(button);
+            });
+        });
+    },
+
+    removeItem(button) {
+        console.log(button);
     }
 };
+
+addToCart.displayCart();
 
 export default addToCart;
