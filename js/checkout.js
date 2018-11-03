3a42abf6d8a365caf35b16125120af6a58b932c4
Kristin Hortsch <kristinhortsch@gmail.com>
2018-11-02 17:09:05 -0700

    order summary displays not in one line

--- js/checkout.js
+++ js/checkout.js
@@ -2,14 +2,14 @@ import cartApi from './cart-api.js';
 
 let cart = cartApi.get();
 const orderSummary = document.getElementById('summary');
-let orders = [];
+
 const getCart = {
     display(cart) {
         for(let i = 0; i < cart.length; i++) {
-            orders.push(cart[i].name + ' ' + cart[i].price + ' ' + cart[i].quantity);
-            for(let i = 0; i < orders.length; i++) {
-                orderSummary.textContent = orders;
-            }    
+            const item = document.createElement('li');
+            item.textContent = cart[i].name + ' ' + cart[i].price + ' ' + cart[i].quantity;
+            orderSummary.appendChild(item);
+            console.log(orderSummary);
         }
     }
 };
