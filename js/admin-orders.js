9e685ad612f0cddc016b032f0fb53650a1dc6a2f
Kristin Hortsch <kristinhortsch@gmail.com>
2018-11-02 21:25:47 -0700

    orders and customer info now show on admin-order page

--- js/admin-orders.js
+++ js/admin-orders.js
@@ -1,4 +1,7 @@
 import customersApi from './customer-api.js';
+import cartApi from './cart-api.js';
+
+let cart = cartApi.get();
 
 let customers = customersApi.getAll(); 
 const customerOrders = document.getElementById('customer-orders');
@@ -7,10 +10,16 @@ const getCustomers = {
     display(customers) {
         for(let i = 0; i < customers.length; i++) {
             const li = document.createElement('li');
-            li.textContent = customers[i].name + ' ' + customers[i].streetAddress + ' ' + customers[i].city + ' ' + customers[i].state + ' ' + customers[i].zipCode;
-            
+            li.textContent = customers[i].name + ' ' + customers[i].streetAddress + ' ' + customers[i].city;
             customerOrders.appendChild(li);
             console.log(customerOrders);
+            for(let i = 0; i < cart.length; i++) {
+                const li2 = document.createElement('li');
+                li2.textContent += cart[i].name + ' ' + cart[i].price + ' ' + cart[i].quantity;      
+                customerOrders.appendChild(li2);
+            }
+
+
         }
     }
 };
