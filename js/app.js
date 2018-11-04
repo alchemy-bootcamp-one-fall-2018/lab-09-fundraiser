86e75faa3322450477c5129080efb78df59fda2d
Cheri A Portman <cheriportman@hotmail.com>
2018-11-03 21:46:09 -0700

    made list of things left to do for tomorrow's work

--- js/app.js
+++ js/app.js
@@ -1,6 +1,10 @@
 'use strict';
 
 //clear cart on submit form
+//add cart total to cart object
+//display cart total and cust name on order report
+//display details on click
+//product page
 
 //import productsApi from './products-api.js';
 import listOrders from './order-page.js';
