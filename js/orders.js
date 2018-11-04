1bd6c961e237794d3d172cf80d89cdf3879e05cf
‘Michael <pickup.patterns@gmail.com>
2018-11-04 09:00:53 -0800

    told the orders js file to get products from the products api annd told the orders js to get the current shoppong cart items from the cart api and  initialized shopping cart & current list of products

--- js/orders.js
+++ js/orders.js
@@ -3,3 +3,12 @@ import cartApi from './cart-api.js'
 import productList from './product-list.js'
 import shoppingCart from './shopping-cart.js'
 
+const products = productsApi.getProducts();
+const cart = cartApi.get();
+
+shoppingCart.init(cart);
+productList.init(products, function(product) {
+    cartApi.add(product);
+    shoppingCart.update(cart);
+});
+
