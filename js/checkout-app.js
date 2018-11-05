e62d7646b4688b7c1252b3474808fb6bf0131279
Lance Owen Merrill <Lanceom@hawaii.edu>
2018-11-05 15:29:39 -0800

    only first lance of customer info prints on admin page

--- js/checkout-app.js
+++ js/checkout-app.js
@@ -2,14 +2,14 @@ import customerInfo from './customer-listener.js';
 import customerApi from './customer-api.js';
 import shoppingCartTwo from './checkout-cart.js';
 import cartApi from './cart-api.js';
-// import addCart from './test-print.js';
+import addCart from './test-print.js';
 
 const cart = cartApi.get();
-//const customer = customerApi.getAll();
+const customer = customerApi.getAll();
 
 shoppingCartTwo.init(cart);
 
-//addCart.init(cart);
+addCart.init(customer);
 
 customerInfo.init(function(customer) {
     customerApi.add(customer);
--- js/test-print.js
+++ js/test-print.js
@@ -1,11 +1,4 @@
 
-import customerApi from './customer-api.js';
-import cartApi from './cart-api.js';
-
-
-const cart = cartApi.get();
-const customer = customerApi.getAll();
-
 const list = document.getElementById('admin-info');
 
 const addCart = {
@@ -17,13 +10,6 @@ const addCart = {
             li.textContent += customer[i].quantity;
             list.appendChild(li);
         }
-        for(let i = 0; i < cart.length; i++){
-            const li = document.createElement('li');
-            li.textContent += cart[i].name;
-            list.appendChild(li);
-        
-        }
-        customerApi.getAll();
     }
 
 };
