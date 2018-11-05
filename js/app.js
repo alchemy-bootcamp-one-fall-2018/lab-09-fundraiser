ee8e917a2bf312f8b33b0eb254d9292393026845
Tyler Corbett <thetylercorbett@gmail.com>
2018-11-05 13:23:55 -0800

    products page is working again

--- js/app.js
+++ js/app.js
@@ -1,17 +1,12 @@
 import addCookie from './add-cookie.js';
 import cookieApi from './cookies-api.js';
 import cookieList from './cookie-list.js';
-import cookieDetail from './cookie-remove.js';
 
 const cookies = cookieApi.getAll();
 
-cookieDetail.init(function(cookie) {
-    const index = cookieApi.remove(cookie);
-    cookieList.remove(index);
-});
 
 addCookie.init(function(cookie) {
-  // tell the api service first
+    // tell the api service first
     cookieApi.add(cookie);
 
 });
