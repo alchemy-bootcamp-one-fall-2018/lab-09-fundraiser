574eda3a594d482b8e8ba204135297b55a7cf404
Paige Gorry <paigeegorry@gmail.com>
2018-11-05 08:37:49 -0800

    trying to fix travis

--- js/checkout-app.js
+++ js/checkout-app.js
@@ -2,10 +2,10 @@
 import cartApi from './data-apis/cart-api.js';
 import itemForm from './components/checkout.js';
 import shoppingCart from './components/shopping-cart.js';
-import customerApi from './data-apis/customer-api.js';
+// import customerApi from './data-apis/customer-api.js';
 
 const cart = cartApi.get();
-const customers = customerApi.getAll();
+// const customers = customerApi.getAll();
 
 itemForm.init(function(item){
     cartApi.add(item);
