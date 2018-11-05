812d2356dafc5c9241dda2d1c0415b8e87ab57cd
‘Michael <pickup.patterns@gmail.com>
2018-11-05 09:20:35 -0800

    changed title tag

--- js/orders.js
+++ js/orders.js
@@ -11,4 +11,11 @@ productList.init(products, function(product) {
     cartApi.add(product);
     shoppingCart.update(cart);
 });
+//You need to keep this on th order page for the list of products to show up tis functionality will 
+//show up on the shop.html  page
+// productList.init (products, function(product){
+//     cartApi.add(product);
+//     shoppingCart.update(cart);
+
+// });
 
