43528b0bfc2a70858dcb3101df0babe26be80afc
Lance Owen Merrill <Lanceom@hawaii.edu>
2018-11-05 15:31:50 -0800

    prints all info to admin page, but only first item in customer API

--- js/test-print.js
+++ js/test-print.js
@@ -7,7 +7,10 @@ const addCart = {
         for(let i = 0; i < customer.length; i++) {
             const li = document.createElement('li');
             li.textContent += customer[i].name;
-            li.textContent += customer[i].quantity;
+            li.textContent += customer[i].email;
+            li.textContent += customer[i].shipping;
+            li.textContent += customer[i].billing;
+
             list.appendChild(li);
         }
     }
