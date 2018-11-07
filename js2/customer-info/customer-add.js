3df8a18658fd79b48218eb5bfd0b9b851ea84ec4
Connor Frendt <cjacksonfx@yahoo.com>
2018-11-06 21:35:40 -0800

    we getted all the things from the forms and local storaged it

--- js2/customer-info/customer-add.js
+++ js2/customer-info/customer-add.js
@@ -12,7 +12,12 @@ const addCustomer = {
                 street: elements.street.value,
                 city: elements.city.value,
                 state: elements.state.value,
-                zip: elements.zip.value
+                zip: elements.zip.value,
+                nameShipping: elements['name-shipping'].value,
+                streetShipping: elements['street-shipping'].value,
+                cityShipping: elements['city-shipping'].value,
+                stateShipping: elements['state-shipping'].value,
+                zipShipping: elements['zip-shipping'].value
             };
             console.log(newCustomer);
             onAdd(newCustomer);
