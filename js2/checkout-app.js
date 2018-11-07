40a57d94792544bcec9328cb984ab3ce64bc61d4
Connor Frendt <cjacksonfx@yahoo.com>
2018-11-06 22:02:01 -0800

    copy button for addresses stuff working

--- js2/checkout-app.js
+++ js2/checkout-app.js
@@ -11,3 +11,14 @@ cartList.init(cartItems, function(cartItem) {
 addCustomer.init(function(customer) {
     customersApi.add(customer);
 });
+
+var sameInfo = document.getElementById('same-info');
+sameInfo.addEventListener('click', function(event) {
+    event.preventDefault();
+    var form = document.getElementById('billing-info');
+    form.elements['name-shipping'].value = form.elements.name.value;
+    form.elements['street-shipping'].value = form.elements.street.value;
+    form.elements['city-shipping'].value = form.elements.city.value;
+    form.elements['state-shipping'].value = form.elements.state.value;
+    form.elements['zip-shipping'].value = form.elements.zip.value;
+});
--- nextsteps.md
+++ nextsteps.md
@@ -1,6 +1,4 @@
-4 - (Customer Info) - Start with HTML :) Create a (2) form, 1 - billing info, 2 - shipping
 Forms for name, address, etc. CHECKBOX for copying billing info to shipping
-(Checkout) - On form submit, add order (billing, shipping, cust info, quantity) to Local Storage
 (Order Report) - Input table wth customers and orders w/ total cost
 (") Reuse shopping cart, when clicking on picture in order, show item detail
 DUN
